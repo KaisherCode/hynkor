cde55b8c96be9cf6b2678f722b3130ed47d0220f
Wilson Dev <hch.wilson@hynkor.com>
2024-06-26 02:03:59 -0500

    Add paleta de colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		textColorcolor:{
+			'primary':'#00A99D',
+			'secondary':'#F72585',
+			'terciary':'#7209B7',
+			'quarter':'#4361ee',
+			'accent':'#3A0CA3',
+		},
+		backgroundColor: theme=>({
+			...theme('colors'),
+			'primary':'#00A99D',
+			'secondary':'#F72585',
+			'terciary':'#7209B7',
+			'quarter':'#4361ee',
+			'accent':'#3A0CA3',
+		}),
 		extend: {},
 	},
 	plugins: [],
